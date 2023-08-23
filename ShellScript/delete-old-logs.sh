239ba1d56e120dff9aa2801b6644720e0f65cc9f
lakshman <lr944944@gmail.com>
2023-08-23 18:06:43 +0200

    added shell script

--- ShellScript/delete-old-logs.sh
+++ ShellScript/delete-old-logs.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-Date=$(date +%F)
+Date=$(date +%F:%H:%M:%S)
 script=$0
 logdir=/home/centos/app-logs
 logfile=$logdir/$script-$Date.log
 delete=$(find $logdir -name "*.log" -type f -mtime +14)
-echo "$delete"
-
+echo "script started executing at $Date"
 while read line
 do
  echo "Deleting file $line" &>> $logfile
