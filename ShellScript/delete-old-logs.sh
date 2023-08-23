a38ef7ab603e5f80d6a5f6eb3b99cb3058d8d1f3
lakshman <lr944944@gmail.com>
2023-08-23 17:49:30 +0200

    added shell script

--- ShellScript/delete-old-logs.sh
+++ ShellScript/delete-old-logs.sh
@@ -7,6 +7,6 @@ logfile=$logdir/$script-$Date.log
 delete=$(find $logdir -name "*.log" -type f -mtime +14)
 while read line
 do
- echo "Deleting file $line" &> $logfile
+ echo "Deleting file $line" &>> $logfile
  rm -rf $line
 done <<< $delete
