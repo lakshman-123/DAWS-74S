72071fbdc647870a17101210312dddeac2c1b205
lakshman <lr944944@gmail.com>
2023-08-23 16:50:25 +0200

    added shell script

--- ShellScript/for-loop.sh
+++ ShellScript/for-loop.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+Date=$(date +%F:%H:%M:%S)
+script=$0
+logdir=/home/centos/shellscript-logs
+log=$logdir/$script-$Date.log
 userid=$(id -u)
 R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
 if [ $userid -ne 0 ] 
@@ -9,8 +14,22 @@ then
   echo -e "$R Error not root user $N"
   exit 1
 fi 
-
+validate() {
+    if [ $1 -ne 0 ]
+    then
+      echo " error $2 installation .. $R failure $N"
+    else
+      echo " $2 installation .. $G success $N
+}
 for i in $@
 do
- yum install $i -y
+ yum list installed $i
+ if [ $? -ne 0 ]
+ then
+  echo "$i is not installed,let's install it"
+  yum install $i -y &>>$log
+  validate $? "$i"
+ else
+  echo "$Y $i is already installed! $N" 
+ fi
 done 
