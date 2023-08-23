2e887dfbf152554b9f74f754500720d9e7494128
lakshman <lr944944@gmail.com>
2023-08-23 11:42:58 +0200

    added shell script

---
+++ ShellScript/log.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+Date=$(date +%F:%H:%M:%S)
+logfile=
+userid=$(id -u)
+validate() {
+    if [ $1 -ne 0 ]
+then
+ echo "$2 is failure"
+ exit 1
+else
+ echo "$2 is success"
+fi 
+
+}
+
+if [ $userid -ne 0 ]
+then
+ echo "This is not root user"
+ exit 1
+fi
+
+yum install -y mysql
+validate $? "mysql installing"
+
+yum install postfix -y
+validate $? "postfix installing"
---
+++ ShellScript/special-vars.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+var1=$1
+var2=$2
+
+validate() {
+    echo "$1 is $2"
+}
+
+validate "var1" $var1
+validate  "all vars" $@
+validate  "no.of vars" $#
+
+
