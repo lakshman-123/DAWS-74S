5fe90a29e4497a8046d20f7cd9384e67a090f422
lakshman <lr944944@gmail.com>
2023-08-24 21:06:28 +0200

    added shell script

--- ShellScript/mail.sh
+++ ShellScript/mail.sh
@@ -6,4 +6,7 @@ BODY=$3
 TEAM_NAME=$4
 ALERT_TYPE=$5
 
-echo "all args: $@"
+#echo "all args: $@"
+FINAL_BODY=$(sed -e 's/TEAM_NAME/DEVOPS_TEAM/g' -e 's/ALERT_TYPE/High Disk Usage/g' -e "s/ALERT_TYPE/$BODY" template.html)
+
+echo "$FINAL_BODY" | mail -s "$SUBJECT" $TO_ADDRESS
