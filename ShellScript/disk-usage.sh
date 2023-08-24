9d0a2080d83f016b0723f963f9a1d77791cf0340
lakshman <lr944944@gmail.com>
2023-08-24 16:33:10 +0200

    added shell script

--- ShellScript/disk-usage.sh
+++ ShellScript/disk-usage.sh
@@ -12,6 +12,6 @@ disk_threshold=1
 
 while IFS= read line
 do
-  usage=$( $line|awk '{print $6}'| cut -d % -f1 )
-  echo "$usage"
+  usage=$($line | awk '{print $6}' | cut -d % -f1)
+  
 done <<< $disk_usage
